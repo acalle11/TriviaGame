be44743ef2062e8bba7e7b32c0126384c16481eb
acalle11 <acalle11@gmail.com>
2018-08-28 12:29:29 -0500

    resubmitted

---
+++ assets/javascript/questions.js
@@ -0,0 +1,174 @@
+var panel = $("#quiz-area");
+
+// Question set
+var questions = [{
+  question: "Who won the first Superbowl?",
+  answers: ["Green Bay Packers", "Chicago Bears", "Kansas City Chiefs", "New York Jets"],
+  correctAnswer: "Green Bay Packers"
+}, {
+  question: "Which team has won the most superbowls?",
+  answers: ["Steelers", "Browns", "49ers", "Patriots"],
+  correctAnswer: "Steelers"
+}, {
+  question: "Who is the only NFL team in the superbowl era to go undefeated?",
+  answers: ["Lions", "Broncos", "Rams", "Dolphins"],
+  correctAnswer: "Dolphins"
+}, {
+  question: "Which team has the most hall of famers?",
+  answers: ["Bears", "Packers", "Giants", "Cardinals"],
+  correctAnswer: "Cardinals"
+}, {
+  question: "Which NFL qb dated Olivia Munn?",
+  answers: ["Tom Brady", "Jay Cutler", "Aaron Rodgers", "Cam Newton"],
+  correctAnswer: "Aaron Rodgers"
+}, {
+  question: "What year was the first superbowl played?",
+  answers: ["1000 BC", "1954", "1968", "1967"],
+  correctAnswer: "1967"
+}, {
+  question: "Who is the best team in the NFL?",
+  answers: ["Packers", "Eagles", "Patriots", "Bears"],
+  correctAnswer: "Bears"
+}, {
+  question: "Who is the worst team in the NFL?",
+  answers: ["Packers", "Browns", "Bills", "Lions"],
+  correctAnswer: "Packers"
+}];
+
+// Variable that will hold the setInterval
+var timer;
+
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
